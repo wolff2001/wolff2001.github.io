88f5310e8dc05fedd713c969a587bba5ba6bdc6c
wolff2001 <william.beasley001@gmail.com>
2020-11-23 17:58:29 -0500

    k

--- update_index.py
+++ update_index.py
@@ -16,8 +16,42 @@ def anyfunction(x:float,y:int):
     """
     return x*y
 
+
 def readfile(filename:str):
+    """reads a file, takes in filename
+
+    Args:
+        filename (str): [filepointer]
+
+    Returns:
+        str: the html string
+    """
     with open(filename, "r") as file_handle:
         file_contents = file_handle.read()
+
+        return file_contents
+
+def create_index(filename:str,file_content:str):
+    with open(filename, "w") as file_handle:
+        file_handle.write(file_content)
         
+import pandas as pd
+import random as r
+
+x=list(range(1,11))
+y=[r.randint(1,100) for i in x]
+
+data={
+    
+    'x':x,
+    'y':y
+}
   
+df=pd.DataFrame(data)  
+
+
+header_content=readfile(web['header'])
+footer_content=readfile(web['footer'])
+web['content']=df.to_html(index=False,table_id='sales')
+index_content=header_content+web['content']+footer_content
+create_index('index.html',index_content)
